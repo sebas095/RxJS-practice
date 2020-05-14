47448be6732ca79d88cda3211be56af517c23ac1
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-05-14 03:30:07 -0500

    [DEL] file removed

--- day-01/day-01.js
+++
@@ -1,6 +0,0 @@
-//RxJS
-const button = document.querySelector("button");
-const obs$ = rxjs.fromEvent(button, "click");
-
-const a = obs$.subscribe(() => console.log("Clicked RxJS!"));
-const b = obs$.subscribe(() => alert("hey"));
