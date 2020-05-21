c5d975f5662d8db8382b3e7a00a53ee3b652ef27
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-05-21 01:27:12 -0500

    [ADD] day 11 - BehaviorSubject

---
+++ day-11/example.js
@@ -0,0 +1,15 @@
+// https://medium.com/@jorgeucano/30-d%C3%ADas-con-rxjs-d%C3%ADa-11-bf99fc2c683a
+const subject = new rxjs.BehaviorSubject(0); // valor inicial
+
+subject.subscribe({
+  next: (v) => console.log("Observer A " + v),
+});
+
+subject.next(1);
+subject.next(2);
+
+subject.subscribe({
+  next: (v) => console.log("Observer B " + v),
+});
+
+subject.next(3);
